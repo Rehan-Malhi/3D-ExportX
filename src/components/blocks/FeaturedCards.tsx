b900f2a575291d3e765ac4d5069497c0ceb545d5
Rehan Malhi <engr.rehanmalhi@gmail.com>
2025-08-07 21:17:25 +0500

    Third

--- src/components/blocks/FeaturedCards.tsx
+++ src/components/blocks/FeaturedCards.tsx
@@ -5,7 +5,7 @@ const FeaturedCards = () => {
   return (
     <div className='flex flex-col gap-16'>
       <h1 className='text-5xl'>Featured Jackets</h1>
-      <div className='grid grid-cols-3 place-items-center'>
+      <div className='grid grid-cols-1 place-items-center sm:grid-cols-3 gap-4'>
         <Card title="Jacket 1" src="/immg.jpg" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil." />
         <Card title="Jacket 1" src="/immg.jpg" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil." />
         <Card title="Jacket 1" src="/immg.jpg" description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil." />
